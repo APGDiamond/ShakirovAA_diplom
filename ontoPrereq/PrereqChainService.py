474486acf47b18efcd9015612653511a5a183e87
Almaz Shakirov <shalmaz1733@gmail.com>
2023-06-21 10:25:49 +0300

    add ontoprereq

---
+++ ontoPrereq/PrereqChainService.py
@@ -0,0 +1,53 @@
+from owlready2 import *
+
+
+class PrereqChainService:
+    def __init__(self):
+        self.onto_url = 'https://raw.githubusercontent.com/APGDiamond/ShakirovAA_diplom/main/ontology/ontomathedu.rdf'
+        self.onto = get_ontology(self.onto_url).load()
+        self.chain = []
+
+    def get_prereq_name(self, iri):
+        if len(iri.hasPrerequisite) == 0:
+            if len(iri.hasPrerequisiteRu) == 0:
+                return None
+            else:
+                return iri.hasPrerequisiteRu[0]
+        else:
+            return iri.hasPrerequisite[0]
+
+    def chain_add(self, name):
+        self.chain.append(name)
+        return
+
+    def search_label(self, name):
+        iri = self.onto.search_one(label=name)
+        return iri
+
+    def print_chain(self):
+        res = ""
+        for i in range(len(self.chain)):
+            res += self.chain[i]
+            if i != len(self.chain) - 1:
+                res += " → "
+        print(res)
+        return
+
+    def chain_build(self, name):
+        if name is None:
+            return
+        a = self.search_label(name)
+        if a is not None:
+            self.chain_add(name)
+        else:
+            print("В онтологии отсутствует данный концепт")
+            return
+        prereq = self.get_prereq_name(a)
+        self.chain_build(prereq)
+
+    def prereq_count(self):
+        onto_results = self.onto.search(hasPrerequisite='*')
+        onto_results_ru = self.onto.search(hasPrerequisiteRu='*')
+        return len(onto_results) + len(onto_results_ru)
+
+
---
+++ ontoPrereq/main.py
@@ -0,0 +1,6 @@
+import PrereqChainService
+
+a = PrereqChainService.PrereqChainService()
+
+a.chain_build('Основание высоты треугольника')
+a.print_chain()
